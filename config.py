b01d14b3d72ba56e6012bddccda8a5a207e9cfac
cosentinoandrea768-sudo <cosentinoandrea768@gmail.com>
2026-02-28 13:23:43 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = "IL_TUO_TOKEN_BOT"
+BOT_TOKEN = "IL_TUO_TOKEN_BOT"
 CHAT_ID = "IL_TUO_CHAT_ID"
 CSV_FILE = "events.csv"
 TIMEZONE_OFFSET = 1  # ore rispetto a UTC, per convertire date in locale
