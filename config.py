e1d034b421bdd0e4e21ddeff27f296f01b8b771a
cosentinoandrea768-sudo <cosentinoandrea768@gmail.com>
2026-02-28 14:51:37 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
-BOT_TOKEN = "BOT_TOKEN"
-CHAT_ID = "CHAT_ID"
+import os
+
+# Legge le environment variables di Render
+BOT_TOKEN = os.environ.get("BOT_TOKEN")  # token reale del bot
+CHAT_ID = os.environ.get("CHAT_ID")      # chat ID reale
 CSV_FILE = "events.csv"
-TIMEZONE_OFFSET = 1  # ore rispetto a UTC, per convertire date in locale
+TIMEZONE_OFFSET = 1  # ore rispetto a UTC
